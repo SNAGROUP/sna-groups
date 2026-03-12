629bd0d8db9ebab22fe66f1f191fd3399b2a118f
SNAGROUP <admin@snagroups.com>
2026-03-12 15:09:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-function login(){
-alert("Login system coming soon");
+
+function setColor(c){
+document.documentElement.style.setProperty('--main',c)
 }
+
+function setThai(){
+
+document.getElementById("siteTitle").innerText="ศูนย์บริการ SNA GROUPS"
+
+document.querySelector('[data-edit="service1"]').innerText="บริการแปลเอกสาร"
+
+document.querySelector('[data-edit="service2"]').innerText="บริการตรวจคนเข้าเมือง"
+
+document.querySelector('[data-edit="service3"]').innerText="ภาษีที่ดินและสิ่งปลูกสร้าง"
+
+}
+
+function setEng(){
+
+document.getElementById("siteTitle").innerText="SNA GROUPS Service Portal"
+
+document.querySelector('[data-edit="service1"]').innerText="Translation Service"
+
+document.querySelector('[data-edit="service2"]').innerText="Immigration Service"
+
+document.querySelector('[data-edit="service3"]').innerText="Land & Building Tax"
+
+}
+
+window.onload=setEng
